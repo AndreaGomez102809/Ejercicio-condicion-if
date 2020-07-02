e18084f4641b85291350f83692edeaed70e16f69
unknown <magomez1028@gmail.com>
2020-07-02 16:34:02 -0500

    Ejercicio en clase condicion if

---
+++ js/index.js
@@ -0,0 +1,35 @@
+
+// var a = 5+8
+var matriz = [['q',0,0,0],
+[0,'u',0,0],
+[0,0,'e',0],0,5,33];
+
+var one = "es una prueba1";
+var two = "es otra prueba2";
+var three = "prueba";
+
+
+
+if(one.indexOf(two)!=-1){
+    console.log("la variable two esta contenida en one");
+}
+
+if(one.indexOf(three)!=-1){
+    console.log("la variable three esta contenida en one");
+}
+
+if(two.indexOf(one)!=-1){
+    console.log("la variable one esta contenida en two");
+}
+
+if(two.indexOf(three)!=-1){
+    console.log("la variable three esta contenida en two");
+}
+
+if(three.indexOf(one)!=-1){
+    console.log("la variable one esta contenida en three");
+}
+
+if(three.indexOf(two)!=-1){
+    console.log("la variable three esta contenida en two");
+}
